4639693931d672a4594b7c496728c8e8433aeb0a
Jinxzzl <Azazel.dev@tuta.io>
2021-11-01 18:44:07 +0300

    Update Content
    + Added section "Best Quality Products".
    + Added script for for select "Best Quality Products" filter (style only).
    + Simple changed "category" styles.
    
    (Minor header change: The submenu has been move to bottom by 3px)

--- src/client/public/js/script.js
+++ src/client/public/js/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOOM LOADED');
-    //script for future interactions
+    document.querySelectorAll('.select-items').forEach(node => {
+        const node_items = node.querySelectorAll('.select-item > a')
+        for (const click_item of node_items){
+            click_item.addEventListener('click', event => {
+                for (let item of node_items){
+                    item != event.currentTarget ? item.parentElement.classList.remove('active') : item.parentElement.classList.add('active');
+                };
+            });
+        };
+    });
 });
